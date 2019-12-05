424c84ce8fe3e40ef1a7e53ea8c945a04c166a8e
changlizhi <changlizhi@aliyun.com>
2019-12-05 11:27:19 +0800

    first test

--- jiekou.go
+++ jiekou.go
@@ -1,22 +1,13 @@
-package 2wenjianbaocun
-import(
-  "github.com/gin-gonic"
-  "github.com/json-iterator/go"
-)
-func main() {
-	router := gin.Default()
-	// 为 multipart forms 设置较低的内存限制 (默认是 32 MiB)
-	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
-	router.POST("/upload", func(c *gin.Context) {
-		// 单文件
-		file, _ := c.FormFile("file")
-		log.Println(file.Filename)
-    var json = jsoniter.ConfigCompatibleWithStandardLibrary
-    json.Marshal(&file.Filename)
-		// 上传文件至指定目录
-		// c.SaveUploadedFile(file, dst)
+package main
 
-		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-	router.Run(":8080")
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
