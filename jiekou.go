6c60f2e2da936d7d8259565b5dbd90748ca05122
changlizhi <changlizhi@aliyun.com>
2019-12-29 01:02:56 +0800

    jiekou

--- jiekou.go
+++ jiekou.go
@@ -12,12 +12,30 @@ type Tone struct{
   Name string
   Age int64
 }
+////// 跨域
+func Cors() gin.HandlerFunc {
+  return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		//放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		// 处理请求
+		c.Next()
+	}
+}
 func main() {
 	router := gin.Default()
-
+  router.Use(Cors())
   var json = jsoniter.ConfigCompatibleWithStandardLibrary
   var data =Tone{}
-
+  //router.OPTIONS("/upload", func(c *gin.Context){} )
   router.POST("/upload", func(c *gin.Context) {
 		// Multipart form
 		form, _ := c.MultipartForm()
@@ -43,5 +61,5 @@ func main() {
 
     fmt.Printf("\nids: %v;\n names: %v\n", ids, names)
 	})
-	router.Run(":8080")
+	router.Run(":8081")
 }
